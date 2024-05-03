c60b8f7b60f63d6ba7e4cfc0209a4c47f116df71
HTML Kicj <62546807+htmlkick@users.noreply.github.com>
2024-05-03 11:52:23 +0000

    DataTypes

--- basics/02_dataTypes.js
+++ basics/02_dataTypes.js
@@ -1,1 +1,27 @@
-console.log = ("Rakesh Kumar");
+"use strict"; /* treat as new code */
+
+//alert ("30+30") // we use alert in browser not in files
+
+console.log( 30 + 30);
+console.log("Rakesh Kumar");  /* use clean code practice for readability*/
+
+
+let name ="Rakesh"
+let age = 25
+let isloggedin = false
+let state;
+
+// number => 2 power of 53
+// bigint
+// string => ""
+// boolean => ture/false
+// null => standalone value
+// undifined 
+// symbol => unique
+
+
+// object
+console.log(typeof null); // null is object
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isloggedin);
